88015f867a0fc1f6cf59585f27a2f4158bb8b1be
tumnus123 <tumnus123@gmail.com>
2013-12-24 18:40:51 -0800

    Stub of FractalLandscape with array constructor

--- AndroidFractalLandscape/src/com/seethefractals/androidfractallandscape/FractalLandscape.java
+++ AndroidFractalLandscape/src/com/seethefractals/androidfractallandscape/FractalLandscape.java
@@ -2,7 +2,63 @@ package com.seethefractals.androidfractallandscape;
 
 public class FractalLandscape
 {
-	public int func01(int input) {
-		return input;	
+	public int flSize;
+	public float[][] flArray;
+	private float spacing;
+	private float mag;
+	private float[] centerXY;
+
+	public FractalLandscape(int iSize) {
+		flArray = new float[iSize][iSize];
+		flSize = iSize;
+		init();
+		centerXY = new float[] {0.0f,0.0f};
 	}
+
+	public void setCenterXY(float[] centerXY)
+	{
+		this.centerXY = centerXY;
+	}
+
+	public float[] getCenterXY()
+	{
+		return centerXY;
+	}
+
+	public void setFlMag(float flMag)
+	{
+		this.mag = flMag;
+	}
+
+	public float getFlMag()
+	{
+		return mag;
+	}
+
+	public float getFlSpacing()
+	{
+		return spacing;
+	}
+	
+	public void init() {
+		for(int i=0; i<flSize; i++) {
+			for(int j=0;j<flSize;j++) {
+				flArray[i][j] = 0.0f;
+			}
+		}
+	}
+	
+	public float getHeightAtIndex(int i, int j) {
+		return flArray[i][j];
+	}
+	
+	public void setFlSize(int flSize)
+	{
+		this.flSize = flSize;
+	}
+
+	public int getFlSize()
+	{
+		return flSize;
+	}	
 }
--- AndroidFractalLandscape/src/com/seethefractals/androidfractallandscape/MainActivity.java
+++ AndroidFractalLandscape/src/com/seethefractals/androidfractallandscape/MainActivity.java
@@ -22,12 +22,25 @@ public class MainActivity extends Activity
 				public void onClick(View p1)
 				{
 					// Tests
-					FractalLandscape fl = new FractalLandscape();
-
-					showToast("" + fl.func01(123));
-					
 					StringBuilder sb = new StringBuilder();
-					sb.append("" + fl.func01(123));
+					
+					// Test01: 
+					// New landscape, 10x10 array of 0.0f values
+					// Centered at 0f,0f
+					FractalLandscape fl = new FractalLandscape(10);
+					boolean bTest01 = true;
+					for(int i=0; i<10; i++) {
+						for(int j=0;j<10;j++) {
+							if(fl.getHeightAtIndex(i,j)!=0.0f) {
+								bTest01 = false;
+							}
+						}
+					}
+					float[] centerXY = fl.getCenterXY();
+					if((centerXY[0]!=0.0f)||(centerXY[1]!=0.0f)) {
+						bTest01 = false;
+					}
+					sb.append("Test01: " + bTest01);
 					updateText(sb);
 					
 				}
